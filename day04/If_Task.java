0c17d2f357a3d9654c395a7d46b6635727f08f83
mineqwwe <mineqwwwa@outlook.com>
2023-08-30 13:35:08 +0800

    deleted:    day04/.gitkeep new file:   day04/If_Task.java

---
+++ day04/If_Task.java
@@ -0,0 +1,22 @@
+public class If_Task{
+    public static void main(String[] args){
+
+        int newPhone = 7988;
+        int oldOphone = 1500;
+        float discount = 0.8f;
+
+        float notGiveCost = newPhone - oldOphone;
+        float doGiveCost = newPhone*discount;
+
+        if(notGiveCost < doGiveCost){
+            System.out.println("不使用以旧换新");
+        }else if(notGiveCost > doGiveCost){
+            System.out.println("使用以旧换新");
+        }else{
+            System.out.println("都可以");
+        }
+        System.out.println("不使用以旧换新："+notGiveCost);
+        System.out.println("使用以旧换新："+doGiveCost);
+
+    }
+}
