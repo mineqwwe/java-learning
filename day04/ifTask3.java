5d1186a9298a377d46a1ed43dca61082ce2f5d2c
mineqwwe <mineqwwwa@outlook.com>
2023-08-30 20:47:41 +0800

    day04/ifTask3.java

---
+++ day04/ifTask3.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class ifTask3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println(" 输入本金：");
+        int base = sc.nextInt();
+        System.out.println(" 输入年限：");
+        int year = sc.nextInt();
+
+        if (base >= 1000) {
+            if (year == 1) {
+                double all = base + base * 0.0225 * year;
+                System.out.println(" 本息总额=" + all);
+
+            } else if (year == 2) {
+                double all = base + base * 0.0270 * year;
+                System.out.println(" 本息总额=" + all);
+
+            } else if (year == 3) {
+                double all = base + base * 0.0325 * year;
+                System.out.println(" 本息总额=" + all);
+
+            } else if (year == 5) {
+                double all = base + base * 0.0360 * year;
+                System.out.println(" 本息总额=" + all);
+
+            } else {
+                System.out.println(" 不支持的年限");
+            }
+
+        } else {
+            System.out.println(" 本金太少");
+        }
+
+    }
+}
