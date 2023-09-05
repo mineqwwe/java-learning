8abbb9ca201eeec7a68600e4e05b4d4cc02717fa
mineqwwe <mineqwwwa@outlook.com>
2023-09-05 21:34:13 +0800

    day05/arrayTask7.java

---
+++ day05/arrayTask7.java
@@ -0,0 +1,30 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class arrayTask7 {
+    public static void main(String[] args) {
+        int[] arr = new int[10];
+        Random r = new Random();
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = r.nextInt(100) + 1;// 1 ~ 100
+        }
+        int temp = arr[0];
+        int ji = -1;
+        for (int i = 0; i < arr.length; i++) {
+
+            if (arr[i] % 2 != 0) {
+                temp = arr[i];
+                arr[i] = arr[ji + 1];
+                arr[ji + 1] = temp;
+                ji++;
+            }
+
+        }
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+
+        }
+
+    }
+
+}
