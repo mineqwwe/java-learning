c5efffb4b2c7314f6d16f0f987e123f1ab806e5c
mineqwwe <mineqwwwa@outlook.com>
2023-09-05 20:40:52 +0800

    day05/arrayTask2.java day05/arrayTask3.java

---
+++ day05/arrayTask2.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class arrayTask2 {
+    public static void main(String[] args) {
+        double[] arr = { 12.9, 53.54, 75.0, 99.1, 3.14 };
+        double min = arr[0];
+        for (int i = 0; i < arr.length; i++) {
+            min = min > arr[i] ? arr[i] : min;
+        }
+        System.out.println(min);
+    }
+
+}
---
+++ day05/arrayTask3.java
@@ -0,0 +1,22 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class arrayTask3{
+    public static void main(String[] args){
+       int[] arr = new int[6];
+       Random r = new Random();
+       for (int i = 0; i < arr.length; i++) {
+        arr[i] = r.nextInt(100);
+        System.out.println(arr[i]);
+        
+       }
+
+       int sum = 0;
+       for (int i = 0; i < arr.length; i++) {
+        sum = sum + arr[i];
+        
+       }
+       System.out.println("sum:" + sum);
+    }
+
+}
