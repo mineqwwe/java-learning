6f729543973eaeaef8421e311a861e3e69eef422
mineqwwe <mineqwwwa@outlook.com>
2023-09-11 19:54:53 +0800

    day06/MethodTask6.java

---
+++ day06/MethodTask6.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class MethodTask6 {
+    public static void main(String[] args) {
+        int[] arr1 = { 0, 1, 2, 3 };
+        int[] arr2 = { 0, 2, 2, 3 };
+        boolean equals = equals(arr1, arr2);
+        System.out.println(equals);
+
+    }
+
+    public static boolean equals(int[] arr1, int[] arr2) {
+        if(arr1.length != arr2.length) return false;
+
+        for(int i = 0; i < arr1.length; i++){
+            if(arr1[i] != arr2[i]) return false;
+        }
+        return true;
+    }
+
+
+
+}
