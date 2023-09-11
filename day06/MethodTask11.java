c248c772a13892821c71cbaf7e022c6ee931535e
mineqwwe <mineqwwwa@outlook.com>
2023-09-11 20:50:28 +0800

    day06/MethodTask11.java

---
+++ day06/MethodTask11.java
@@ -0,0 +1,37 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class MethodTask11 {
+    public static void main(String[] args) {
+        int[] arr = { 2, 588, 888, 1000, 10000, 999 };//data can be 0
+        chouKa(arr);
+    }
+
+    public static void chouKa(int[] arr) {
+        Scanner sc = new Scanner(System.in);
+        Random r = new Random();
+        int times = 1;
+        while (true) {
+            System.out.println("按1继续，按0退出");
+            int con = sc.nextInt();
+            if (con == 1) {
+                boolean news = true;
+                while (news) {
+                    int seed = r.nextInt(arr.length);
+                    if (arr[seed] != -1) {
+                        System.out.println(arr[seed] + "元的奖金被抽出");
+                        arr[seed] = -1;
+                        news = false;
+                    }
+                }
+                times++;
+                if (times > arr.length) {
+                    System.out.println("奖金已被全部抽出");
+                    break;
+                }
+            } else {
+                break;
+            }
+        }
+    }
+}
