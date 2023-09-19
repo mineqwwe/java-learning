90b9ed10d872343a2a4fd8dc4a31dc1a3d68ab93
mineqwwe <mineqwwwa@outlook.com>
2023-09-19 21:00:10 +0800

    day08/OOPTask1/Phone.java day08/OOPTask1/PhoneTest.java

---
+++ day08/OOPTask1/Phone.java
@@ -0,0 +1,47 @@
+public class Phone {
+    private String brand;
+    private String color;
+    private int price;
+
+    public Phone() {
+    }
+
+    public Phone(String brand, String color, int price) {
+        this.brand = brand;
+        this.color = color;
+        this.price = price;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public void call() {
+        System.out.println("正在使用价格为" + price + "元" + color + "的" + brand + "手机打电话....");
+    }
+
+    public void sendMessage() {
+        System.out.println("正在使用价格为" + price + "元" + color + "的" + brand + "手机发短信....");
+    }
+
+}
---
+++ day08/OOPTask1/PhoneTest.java
@@ -0,0 +1,17 @@
+public class PhoneTest {
+
+    public static void main(String[] args) {
+        Phone p = new Phone();
+
+        p.setBrand("小米");
+        p.setColor("白色");
+        p.setPrice(1999);
+
+        p.call();
+
+        Phone p2 = new Phone();
+        p2.call();
+
+    }
+
+}
