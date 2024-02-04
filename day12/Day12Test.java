f647da25c9802c4ee205ea69020deb59bf62aad3
mineqwwe <mineqwwwa@outlook.com>
2024-02-04 21:36:18 +0800

    ToolClass.java

---
+++ day12/Day12Test.java
@@ -0,0 +1,8 @@
+public class Day12Test {
+    public static void main(String[] args) {
+        System.out.println(ToolClass.add(1, 2));
+        int div = ToolClass.div(9,2);
+        System.out.println(div);
+    }
+
+}
---
+++ day12/ToolClass.java
@@ -0,0 +1,10 @@
+public class ToolClass {
+    private ToolClass(){}
+
+    public static int add(int a , int b) {
+        return a+b;
+    }
+    public static int div(int a , int b) {
+        return a/b;
+    }
+}
