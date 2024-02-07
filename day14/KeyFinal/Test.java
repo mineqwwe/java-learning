a12b4247acd8fd56d6d61871474d2f2e0319cc17
mineqwwe <mineqwwwa@outlook.com>
2024-02-07 20:25:15 +0800

    KeyFinal

---
+++ day14/KeyFinal/Test.java
@@ -0,0 +1,18 @@
+package day14.KeyFinal;
+
+class Test {
+    public static void main(String[] args) {
+        //final
+        //类：  被修饰的类，不能被继承。
+
+        //方法：被修饰的方法，不能被重写。
+
+        //变量：被修饰的变量，有且仅能被赋值一次。
+
+        //定义常量
+        final double PI = 3.14;
+
+        System.out.println("PI: "+ PI);
+
+    }
+}
